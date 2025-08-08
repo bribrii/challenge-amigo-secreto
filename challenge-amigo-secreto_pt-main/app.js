b08aa10cd64cc05079bb5c8fa65f6848e07c0688
Brian Santos <brian.nascimento1@gmail.com>
2025-08-08 18:58:15 -0300

    adicionando função para a lista

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,2 +1,31 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-testando o commit 
+let amigos = [];
+
+function adicionarAmigo() {
+    let nome = document.querySelector('input').value;
+
+    if (nome == ' ') {
+        alert('Por favor, digite um nome válido para o sorteio!');
+    } else {
+        amigos.push(nome);
+        console.log(amigos);
+    }
+    limparCampo();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+function limparCampo() {
+    nome = document.querySelector('input');
+    nome.value = '';
+}
